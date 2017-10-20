b68e50339e994c28beaab4e469103f772eb0462f
alexey.radkovich <alexey.radkovich@gmail.com>
2017-10-20 19:44:40 +0300

    HTTP

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -10,7 +10,7 @@ import { HeroService } from './hero.service'
 @Component({
     selector: 'hero-detail',
     templateUrl: './hero-detail.component.html',
-    styleUrls: [ './hero-detail.component.css' ]
+    styleUrls: ['./hero-detail.component.css']
 })
 
 export class HeroDetailComponent implements OnInit {
@@ -30,5 +30,10 @@ export class HeroDetailComponent implements OnInit {
 
     goBack(): void {
         this.location.back();
+    }
+
+    save(): void {
+        this.heroService.update(this.hero)
+            .then(() => this.goBack());
     }
 }
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -8,7 +8,7 @@ import { HeroService } from './hero.service';
 @Component({
   selector: 'my-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: [ './heroes.component.css' ],
+  styleUrls: ['./heroes.component.css'],
   providers: [
     HeroService
   ]
@@ -20,9 +20,9 @@ export class HeroesComponent implements OnInit {
 
   constructor(private router: Router, private heroService: HeroService) { }
 
-  getHeroes():void {
+  getHeroes(): void {
     this.heroService.getHeroes().then(heroes => {
-      this.heroes = heroes;  
+      this.heroes = heroes;
     });
   }
 
@@ -36,5 +36,24 @@ export class HeroesComponent implements OnInit {
 
   gotoDetail(): void {
     this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroService
+      .delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        if (this.selectedHero === hero) { this.selectedHero = null; }
+      });
   }
 }
